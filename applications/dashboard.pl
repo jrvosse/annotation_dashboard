025ddc93b84fbe8521936dc818589eb5ef2dc19a
Jacco van Ossenbruggen <Jacco.van.Ossenbruggen@cwi.nl>
2014-05-09 20:21:31 +0200

    wip on dashboard 2

--- applications/dashboard.pl
+++ applications/dashboard.pl
@@ -1,18 +1,27 @@
 :- module(an_dashboard, []).
 
+% from SWI-Prolog libraries:
 :- use_module(library(semweb/rdf_db)).
-:- use_module(library(semweb/rdf_label)).
+:- use_module(library(semweb/rdfs)).
 :- use_module(library(http/http_dispatch)).
 :- use_module(library(http/http_parameters)).
 :- use_module(library(http/html_write)).
 :- use_module(library(http/html_head)).
-:- use_module(library(oa_schema)).
 
-:- use_module(user(user_db)).
+% from ClioPatria:
+:- use_module(library(semweb/rdf_label)).
 :- use_module(cliopatria(hooks)).
+:- use_module(components(label)).
+:- use_module(user(user_db)).
+
+% from other cpacks:
+:- use_module(library(oa_schema)).
+:- use_module(library(oa_annotation)).
+:- use_module(api(media_caching)). % needed for http api handlers
 
 :- http_handler(cliopatria(annotate/dashboard/home), http_dashboard_home, []).
 :- http_handler(cliopatria(annotate/dashboard/user), http_dashboard_user, []).
+:- http_handler(cliopatria(annotate/dashboard/task), http_dashboard_task, []).
 
 :- setting(annotation:dashboard_admin_only, boolean, true,
 	   'Dashboard only for users with admin rights').
@@ -25,9 +34,13 @@ cliopatria:menu_item(100=annotation/http_dashboard_home, 'dashboard').
 		    [ '//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css'
 		    ])
 	      ]).
+:- rdf_meta
+	task_property_blacklist(t).
 
-:- multifile
-	show_user_annotations//3.
+task_property_blacklist(
+    [rdf:type,
+     dcterms:title
+    ]).
 
 http_dashboard_user(Request) :-
 	(setting(annotation:dashboard_admin_only, true)
@@ -40,6 +53,38 @@ http_dashboard_home(_Request) :-
 	-> authorized(admin(dashboard)); true),
 	dashboard_page([]).
 
+http_dashboard_task(Request) :-
+	(setting(annotation:dashboard_admin_only, true)
+	-> authorized(admin(dashboard)); true),
+	http_parameters(Request, [task(Task, [])]),
+	task_page(Task, []).
+
+task_page(Task, _Options) :-
+	rdf_label(Task, Label),
+	find_annotations_by_task(Task, Annotations),
+	maplist(rdf_get_annotation_target, Annotations, Targets),
+	sort(Targets, Objects),
+	reply_html_page([title(Label),
+			 meta([name(viewport),
+			       content('width=device-width, initial-scale=1')]),
+			 \html_requires(
+			      [bootstrap,
+			       css('dashboard.css')
+			      ])],
+			[ \top_navbar,
+			  div([class('container-fluid')],
+			      [ div([class(row)],
+				    [ div([class('col-sm-12 col-md12 col-md-offset-0 main')],
+					  [ h1([class('page-header')], ['Dashboard']),
+					    h2([class('sub-header')],
+					       [Label]),
+					    h3([class('sub-header')],
+					       ['Task objects']),
+					    \show_objects(Objects, Annotations)
+					  ])
+				    ])
+			      ])
+			]).
 
 user_page(User, _Options) :-
 	findall(Prop, user_property(User, Prop), Props),
@@ -58,7 +103,8 @@ user_page(User, _Options) :-
 		  [ div([class(row)],
 			[ div([class('col-sm-3 col-md-2 sidebar')],
 			      [nav_to_be_done]),
-			  div([class('col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main')],
+			  div([class('col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main')
+			      ],
 			      [ h1([class('page-header')], ['Dashboard']),
 				h2([class('sub-header')],
 				   ['User information']),
@@ -77,7 +123,10 @@ user_page(User, _Options) :-
 
 
 dashboard_page(_Options) :-
+	find_tasks(Tasks),
 	find_users(Users),
+	find_annotations_without_task(TaskLess),
+	length(TaskLess, NrTaskless),
 	length(Users, NrOfUsers),
 	reply_html_page(
 	    [ title('Annotation dashboard'),
@@ -96,6 +145,14 @@ dashboard_page(_Options) :-
 			      [nav_to_be_done]),
 			  div([class('col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main')],
 			      [ h1([class('page-header')], ['Dashboard']),
+				\show_tasks(Tasks),
+				h2([class('sub-header')],
+				   ['Total annotations not associated with a defined task: ', NrTaskless]),
+				div([class('table-responsive')],
+				    [table([class('table table-striped')],
+					   [\show_annotations(TaskLess)]
+					  )
+				    ]),
 				h2([class('sub-header')],
 				   ['Total number of users so far: ', NrOfUsers]),
 				div([class('table-responsive')],
@@ -120,6 +177,96 @@ top_navbar -->
 		[nav_to_be_done])
 	).
 
+show_objects([],_) --> !.
+show_objects([H|T],A) -->
+	show_object(H,A),
+	show_objects(T,A).
+
+image(O,O).
+match_target(T,A) :-
+	rdf_get_annotation_target(A,T).
+
+show_object(O,A) -->
+	{ image(O, Image),
+	  http_link_to_id(http_fit_thumbnail, [uri(Image)], Thumbnail),
+	  http_link_to_id(http_original, [uri(Image)], Original),
+	  include(match_target(O), A, Annotations)
+	},
+	html([
+	    div([class('col-md-6')],
+		[a([href(Original)],
+		   [img([class('dashboard object image'), src(Thumbnail)
+		       ])
+		   ]),
+		 div([class(row)], \rdf_link(O, [resource_format(label),max_length(50)]))
+		]),
+	    div([class('col-md-6 table-responsive')],
+		[ table([class('table table-striped')], [
+			\show_annotations(Annotations)
+			])
+		])
+	]).
+
+
+show_tasks([]) --> !.
+show_tasks([H|T]) -->
+	show_task(H),
+	show_tasks(T).
+
+show_task(Task) -->
+	{ rdf_label(Task,Title),
+	  http_link_to_id(http_dashboard_task, [task(Task)], TaskLink),
+	  find_task_properties(Task, Props)
+	},
+	html([h3([class('sub-header')],
+		 [a([href(TaskLink)],Title)]),
+	      div([class('table-responsive')],
+		  [table([class('table table-striped')],
+			 [ thead([
+			       tr([th('Property'), th('Value')])
+			   ]),
+			   tbody([
+			       \show_user_props(Props)
+			   ])
+			 ])
+		  ])
+	     ]).
+
+show_annotations([]) --> !.
+show_annotations([H|T]) -->
+	html(tr(\show_annotation_summery(H))),
+	show_annotations(T).
+
+is_specific_target_annotation(A) :-
+	rdf_has(A, oa:hasTarget, T),
+	rdfs_individual_of(T, oa:'SpecificResource').
+
+find_task_properties(Task, Props) :-
+	find_annotations_by_task(Task, Annotations),
+	include(is_specific_target_annotation, Annotations, SpecAnns),
+	length(Annotations, NrAnnotations),
+	length(SpecAnns, NrSpecAnns),
+	NrObjAnnotations is NrAnnotations - NrSpecAnns,
+
+	task_property_blacklist(PropertyBlackList),
+	findall(Prop,
+		(   rdf_has(Task, P, O),
+		    \+ member(P, PropertyBlackList),
+		    Prop =.. [P,O]
+		),
+		RDFProps),
+	CountProps = [
+	    annotations(literal(NrAnnotations)),
+	    spec_annotations(literal(NrSpecAnns)),
+	    obj_annotations(literal(NrObjAnnotations)),
+	    targets_total('unknown'),
+	    targets_complete('unknown'),
+	    targets_worked_on('unknown'),
+	    targets_untouched('unknown')
+	],
+	append([CountProps, RDFProps], Props).
+
+
 show_users([]) --> !.
 show_users([U|T]) -->
 	show_user(U),
@@ -134,22 +281,30 @@ show_user(U) -->
 	html(tr([td(a([href(UserLink)],['~p'-Uid])),
 		 td([class='an_nr_of_annotations'],Done)])).
 
+find_tasks(Tasks) :-
+	findall(Status-Task, task(Task, Status), Tasks0),
+	keysort(Tasks0, TasksSorted),
+	pairs_values(TasksSorted, Tasks).
 
 find_users(Users) :-
 	findall(User, participant(User), Users0),
 	sort(Users0, Users).
 
+task(Task, Status) :-
+	rdfs_individual_of(Task, ann_ui:'AnnotationTask'),
+	rdf_has(Task, ann_ui:taskStatus, Status).
+
 participant(User) :-
 	current_user(Uid),
 	user_property(Uid, url(URL)),
-	find_annotations(URL, Annotations),
+	find_annotations_by_user(URL, Annotations),
 	length(Annotations, Done),
 	User= [
 	       id(Uid), url(URL), done(Done)
 	      ].
 
 
-find_annotations(User, Annotations) :-
+find_annotations_by_user(User, Annotations) :-
 	findall(A, annotation_by_user(User, A), Anns0),
 	maplist(ann_time, Anns0, APairs),
 	group_pairs_by_key(APairs, AGrouped),
@@ -157,13 +312,53 @@ find_annotations(User, Annotations) :-
 	pairs_values(AGroupedS, Values),
 	append(Values, Annotations).
 
+find_annotations_by_task(Task, Annotations) :-
+	rdf_has(Task, ann_ui:taskUI, UI),
+	rdf_has(UI, ann_ui:fields, FieldList),
+	findall(
+	    A,
+	    (	rdfs_member(Field, FieldList),
+		annotation_by_field(Field, A)
+	    ),
+	    Annotations).
+
+find_annotations_without_task(Annotations) :-
+	findall(A, is_annotation(A), All),
+	maplist(guess_task, All, AllT),
+	group_pairs_by_key(AllT, Grouped),
+	(   member(undefined-Annotations, Grouped)
+	->  true
+	;   Annotations = []
+	),
+	!.
+
+is_annotation(A) :-
+	rdfs_individual_of(A, oa:'Annotation').
+
+guess_task(Ann,Task-Ann) :-
+	rdf_has(Ann, ann_ui:annotationField, Field),
+	rdf_has(UI, ann_ui:fields, FieldList),
+	rdfs_member(Field, FieldList),
+	rdf_has(Task, ann_ui:taskUI, UI),
+	!.
+guess_task(Ann, undefined-Ann).
+
 ann_time(Ann, Time-Ann) :-
-	rdf(Ann, oa:annotated, TimeLit),
+	rdf(Ann, oa:annotatedAt, TimeLit),
 	literal_text(TimeLit, Time).
 
 annotation_by_user(User, Annotation) :-
-	rdf(Annotation, oa:annotator, User).
+	rdf_has(Annotation, oa:annotatedBy, User).
+annotation_by_field(Field, Annotation) :-
+	rdf_has(Annotation, ann_ui:annotationField, Field).
 
+property_key_label(targets_untouched, '# objects without any annotations').
+property_key_label(targets_total,     '# objects to be annotated').
+property_key_label(targets_complete,  '# objects completed').
+property_key_label(targets_worked_on, '# objects with some annotations').
+property_key_label(annotations,       '# total annotations for this task').
+property_key_label(obj_annotations,   '# annotations on the object').
+property_key_label(spec_annotations,  '# annotations on a image region').
 
 show_user_props([]) --> !.
 
@@ -174,12 +369,31 @@ show_user_props([allow(_)|Tail]) -->
 show_user_props([password(_)|Tail]) -->
 	show_user_props(Tail).
 
-show_user_props([Prop|Tail]) --> {
-    Prop =.. [K,V]
-},
-	html(tr([td(K), td(V)])),
+show_user_props([Prop|Tail]) -->
+	{ Prop =.. [K,V],
+	  (   rdf_current_predicate(K)
+	  ->  Key = \rdf_link(K)
+	  ;   property_key_label(K, Key)
+	  ->  true
+	  ;   Key = K
+	  ),
+	  (   rdf_is_literal(V)
+	  ->  Value = V
+	  ;   rdf_subject(V)
+	  ->  Value = \rdf_link(V)
+	  ;   rdf_global_id(_:Value, V)
+	  ->  true
+	  ;   Value = V
+	  )
+	},
+	html(tr([td(Key), td(Value)])),
 	show_user_props(Tail).
 
-show_annotations(User, A, D) --> show_user_annotations(User, A, D),!.
-show_annotations(_, [], []) -->
-	html(div([class('warning no_annotations')],['no annotations yet'])).
+show_annotation_summery(A) -->
+	{ rdf_label(A, Title),
+	  rdf_has(A, oa:annotatedBy, User)
+	},
+	html([
+	    td(a([href(A)], Title)),
+	    td(\rdf_link(User, [format(nslabel)]))
+	]).
