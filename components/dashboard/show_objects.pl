ca554011aec6de1af3383e104d9ecb2fe371c8b8
Jacco van Ossenbruggen <Jacco.van.Ossenbruggen@cwi.nl>
2014-09-21 20:20:10 +0200

    ADDED: use image_annotation hooks to recreate original dashboard functionality

--- components/dashboard/show_objects.pl
+++ components/dashboard/show_objects.pl
@@ -27,44 +27,32 @@ show_objects(Targets,Options) -->
 	},
 	html(
 	    [div([class(row)],
-		 [div([class('col-xs-6')],
-		      [\annotation_page_body([targets(Targets)|NewOptions])
+		 [div([],
+		      [\annotation_page_body([targets(Targets), media_class('col-xs-6')|NewOptions])
 		      ])
 		 ])
 	    ]).
-
 match_target(T,A) :-
 	rdf_get_annotation_target(A,T).
 
-show_object_old(O, Options) -->
-	{ option(annotations(A), Options, []),
-	  include(match_target(O), A, Annotations),
-	  (   ( option(metadata_fields(_), Options),
-		option(ui(_), Options),
-		option(annotation_fields(_), Options))
-	  ->  NewOptions = Options
-	  ;   ( option(task(Task), Options)
-	      ->  true
-	      ;	  member(First, A),
-		  guess_task(First, Task-First)
-	      ),
-	      rdf_has(Task, ann_ui:taskUI, UI),
-	      get_metafields(UI, [], MetadataFields),
-	      get_anfields(UI, [], [], AnnotationFields),
-	      NewOptions = [
-		  ui(UI),
-		  metadata_fields(MetadataFields),
-		  annotation_fields(AnnotationFields) |
-		  Options
-	      ]
-	  )
+image_annotation:html_application_target_info(Options) -->
+	{ option(task(Task), Options),
+	  rdf_has(Task, ann_ui:taskUI, UI),
+	  get_metafields(UI, [], MetadataFields),
+	  get_anfields(UI, [], [], AnnotationFields),
+	  NewOptions = [
+	      ui(UI),
+	      metadata_fields(MetadataFields),
+	      annotation_fields(AnnotationFields) |
+	      Options
+	  ],
+	  option(target(Target), Options),
+	  option(annotations(A), Options, []),
+	  include(match_target(Target), A, Annotations)
 	},
 	html(
-	    [div([class(row)],
-		 [div([class('col-xs-6')],
-		      [\annotation_page_body([target(O)|NewOptions])
-		      ]),
-		  div([class('col-xs-6 table-responsive')],
+	    [div([class(dashboard)],
+		  [ div([class('col-xs-6 table-responsive')],
 		      [ table([class('table table-striped')], [
 				  \show_annotations(Annotations, NewOptions)
 			      ])
